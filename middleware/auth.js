cf373f294ff712139864d2beaec459509b62e996
pakavisavero <67987808+pakavisavero@users.noreply.github.com>
2021-02-27 08:43:31 +0700

    sukses fitur auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -98,6 +98,6 @@ exports.login = function(req, res){
     });
 }
 
-exports.halamanrahasia = function(req, res) {
-    response.ok("Halaman ini hanya untuk user dengan role = 2!");
+exports.halamanrahasia = function (req, res) {
+     response.ok("Halaman ini hanya untuk user dengan role = 2!", res);
 }
