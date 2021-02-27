af36fcad4707088916c03a1ca241f6936d524ec0
pakavisavero <67987808+pakavisavero@users.noreply.github.com>
2021-02-27 07:52:44 +0700

    verifikasi apakah email sudah terdaftar

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+
+var morgan = require('morgan');
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+//parse application / json 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 
+//panggilkan routes
 var routes = require('./routes');
-
 routes(app);
+
+//daftarkan menu routes dari index
+app.use('/auth', require('./middleware'));
 
 app.listen(3000, () => {
     console.log(`Server started on port`);
